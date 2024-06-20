9d8603a8cc82902d098739a2113e57c98f959350
Gohlub <62673775+Gohlub@users.noreply.github.com>
2024-06-19 17:44:16 -0700

    Added world loading from another Kinode process

--- gamelord/gamelord/src/lib.rs
+++ gamelord/gamelord/src/lib.rs
@@ -3,15 +3,21 @@ use kinode_process_lib::{
     println, Address, LazyLoadBlob, Message, http, http::send_ws_push,
 };
 
+use lazy_static::lazy_static;
+use std::sync::RwLock;
+
+
 mod utilities;
 use utilities::valid_position;
 mod gamelord_types;
 use gamelord_types::{Player, Regions, Region, Cube};
-mod fixtures;
-use fixtures::get_region_json;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+//Here is where we store the CURRENT world config
+lazy_static! {
+    static ref WORLD_CONFIG: RwLock<HashMap<Cube, Region>> = RwLock::new(HashMap::new());
+}
 
 
 #[derive(Deserialize, Debug)]
@@ -24,6 +30,16 @@ struct Body {
 struct OuterBody {
     body: Body,
 }
+#[derive(Serialize, Deserialize, Debug)]
+enum WorldConfig{
+    GenerateWorld(Regions),
+    DeleteWorld
+}
+impl WorldConfig {
+    fn parse(bytes: &[u8]) -> Result<WorldConfig, serde_json::Error> {
+        serde_json::from_slice::<WorldConfig>(bytes)
+    }
+}
 
 wit_bindgen::generate!({
     path: "target/wit",
@@ -35,23 +51,6 @@ struct Connection {
     channel_id: u32,
 }
 
-fn load_regions() -> anyhow::Result<HashMap<Cube, Region>> {
-    let config = get_region_json();
-    let mut map_config = HashMap::<Cube, Region>::new();
-    let map_regions: Regions = serde_json::from_value(config)
-        .map_err(|e| {
-            println!("Failed to deserialize regions: {:?}", e);
-            e
-        })?;
-
-    for region in &map_regions.regions {
-        for cube in &region.cubes {
-            map_config.insert(cube.clone(), region.clone());
-        }
-    }
-    Ok(map_config)
-    
-}
 
 fn is_expected_channel_id(
     connection: &Option<Connection>,
@@ -69,14 +68,15 @@ fn is_expected_channel_id(
 
 fn handle_ws_message(
     connection: &mut Option<Connection>,
-     message: Message,
-      regions: &HashMap<Cube, Region>)
-       -> anyhow::Result<()> {
+    message: Message,
+    
+) -> anyhow::Result<()> {
     match serde_json::from_slice::<http::HttpServerRequest>(message.body())? {
         http::HttpServerRequest::WebSocketOpen { channel_id, .. } => {
             println!("WSOPEN channel open: {}", channel_id);
             *connection = Some(Connection { channel_id });
         }
+
         http::HttpServerRequest::WebSocketPush {
             ref channel_id,
             ref message_type,
@@ -98,12 +98,28 @@ fn handle_ws_message(
                             return Ok(());
                         }
                     };
-                    // probably not good
+
+                    // Load the world configuration
+                    let world_config = WORLD_CONFIG.read().unwrap();
+                    if world_config.is_empty() {
+                        let response = serde_json::to_string(&("world not generated")).unwrap();
+                        send_ws_push(
+                            *channel_id,
+                            http::WsMessageType::Text,
+                            LazyLoadBlob {
+                                mime: Some("application/json".to_string()),
+                                bytes: response.into_bytes(),
+                            },
+                        );
+                        println!("World not generated.");
+                        return Ok(());
+                    }
+
                     let body = outer_body.body;
                     let player = body.player;
                     let cube = body.cube;
 
-                    let (response_message, is_valid) = valid_position(regions, &player, &cube);
+                    let (response_message, is_valid) = valid_position(&world_config, &player, &cube);
 
                     let response = serde_json::to_string(&(is_valid, response_message)).unwrap();
 
@@ -125,13 +141,13 @@ fn handle_ws_message(
             }
         }
 
+
         http::HttpServerRequest::WebSocketClose(ref channel_id) => {
             if !is_expected_channel_id(connection, channel_id)? {
                 return Err(anyhow::anyhow!("Unexpected channel ID"));
             }
             *connection = None;
         }
-
         http::HttpServerRequest::Http(_) => {
             return Err(anyhow::anyhow!("Unexpected HTTP request"));
         }
@@ -139,15 +155,50 @@ fn handle_ws_message(
     Ok(())
 }
 
+fn handle_kinode_message(message: &Message) -> anyhow::Result<()> {
+    match WorldConfig::parse(message.body())? {
+        WorldConfig::GenerateWorld(regions) => {
+            let mut world_config = WORLD_CONFIG.write().unwrap();
+            world_config.clear();
+            for region in &regions.regions {
+                for cube in &region.cubes {
+                    world_config.insert(cube.clone(), region.clone());
+                }
+            }
+            println!("World generated with regions: {:?}", regions);
+            Ok(())
+        }
+        WorldConfig::DeleteWorld => {
+            let mut world_config = WORLD_CONFIG.write().unwrap();
+            world_config.clear();
+            println!("World deleted");
+            Ok(())
+        }
+    }
+}
+    
+
+fn is_websocket_message(message: &Message) -> bool {
+    match serde_json::from_slice::<http::HttpServerRequest>(message.body()) {
+        Ok(http::HttpServerRequest::WebSocketOpen { .. }) => true,
+        Ok(http::HttpServerRequest::WebSocketPush { .. }) => true,
+        Ok(http::HttpServerRequest::WebSocketClose { .. }) => true,
+        _ => false,
+    }
+}
+
 fn handle_message(connection: &mut Option<Connection>) -> anyhow::Result<()> {
     let message = await_message()?;
-
     println!(
         "handle_message: {:?}",
         String::from_utf8_lossy(message.body())
     );
-    let regions: HashMap<Cube, Region> = load_regions().unwrap(); 
-    handle_ws_message(connection, message, &regions)?;
+
+    if is_websocket_message(&message) {
+        handle_ws_message(connection, message)?;
+    } else {
+        handle_kinode_message(&message)?;
+    }
     Ok(())
 }
 
