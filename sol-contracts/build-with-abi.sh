88c4439d33a2f3600bf345155f2121758f8cb43c
jurij-jukic <j.jjukicc@gmail.com>
2024-07-24 10:44:05 -0700

    fix build-with-abi.sh

--- sol-contracts/build-with-abi.sh
+++ sol-contracts/build-with-abi.sh
@@ -2,6 +2,6 @@
 
 forge build
 # Extract the 'abi' field from the source JSON and write it to the target file
-jq '.abi' out/Counter.sol/Counter.json > ../gamelord/gamelord/abi/Counter.json
-jq '.abi' out/Counter.sol/Counter.json > ../mcclient/ui/src/abi/Counter.json
+cp out/Counter.sol/Counter.json ../gamelord/gamelord/abi/Counter.json
+cp out/Counter.sol/Counter.json ../mcclient/ui/src/abi/Counter.json
 echo "ABI updated successfully."
