3f92ced713a870360fa7c89eabd1564651257d37
jurij-jukic <j.jjukicc@gmail.com>
2024-07-24 10:35:07 -0700

    build with abi

--- sol-contracts/build-with-abi.sh
+++ sol-contracts/build-with-abi.sh
@@ -2,7 +2,6 @@
 
 forge build
 # Extract the 'abi' field from the source JSON and write it to the target file
-jq '.abi' out/Counter.sol/Counter.json > ../gamelord/abi/Counter.json
-# cd ~/kinode-work/mc-extension/sol-contracts
-jq '.abi' out/Counter.sol/Counter.json > ../mcclient/ui/abi/Counter.json
+jq '.abi' out/Counter.sol/Counter.json > ../gamelord/gamelord/abi/Counter.json
+jq '.abi' out/Counter.sol/Counter.json > ../mcclient/ui/src/abi/Counter.json
 echo "ABI updated successfully."
